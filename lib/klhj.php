6b00f5021cc156faa9639d8482f8366deb59b6cf
alon1368 <108705033+alon1368@users.noreply.github.com>
2025-10-30 12:01:36 +0800

    Add files via upload

---
+++ lib/klhj.php
@@ -0,0 +1,32 @@
+{
+    "class": [
+        {
+            "type_name": "电影天堂克隆",
+            "type_id": "0"
+        },
+        {
+            "type_name": "黑木耳采集克隆",
+            "type_id": "1"
+        },
+        {
+            "type_name": "金鹰采集克隆",
+            "type_id": "2"
+        },
+        {
+            "type_name": "索尼采集克隆",
+            "type_id": "3"
+        },
+        {
+            "type_name": "华为吧采集克隆",
+            "type_id": "4"
+        },
+        {
+            "type_name": "暴风采集克隆",
+            "type_id": "5"
+        },
+        {
+            "type_name": "ikun采集克隆",
+            "type_id": "6"
+        }
+    ]
+}
